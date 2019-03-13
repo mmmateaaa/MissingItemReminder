5d51946bff45355035ac5e3290c46c3909782a93
Matea <sim.mat06@gmail.com>
2019-03-13 14:47:26 +0100

    Init

---
+++ app/src/main/java/mat06/sim/missingitemreminder/MainActivity.java
@@ -0,0 +1,27 @@
+package mat06.sim.missingitemreminder;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Spinner;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.s_category)
+    Spinner spinner;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
