5975037492274021ccafebeda3d42a61fb3747b2
Matea <sim.mat06@gmail.com>
2019-04-08 10:17:00 +0200

    Add Item Activity

--- app/src/main/java/mat06/sim/missingitemreminder/AddItemActivity.java
+++ app/src/main/java/mat06/sim/missingitemreminder/AddItemActivity.java
@@ -6,7 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Spinner;
 
+import java.util.List;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.realm.Realm;
+import mat06.sim.missingitemreminder.database.RealmDatabase;
+import mat06.sim.missingitemreminder.models.CategoryItem;
+import mat06.sim.missingitemreminder.models.MissingItem;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -25,9 +32,56 @@ public class AddItemActivity extends AppCompatActivity {
     @BindView(R.id.s_category)
     Spinner spinner;
 
+    private MissingItem missingItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        ButterKnife.bind(this);
+
+        populateView();
     }
+
+    private void populateView() {
+        if (missingItem.getName() != null)
+            etName.setText(missingItem.getName());
+        if (missingItem.getDescription() != null)
+            etDescription.setText(missingItem.getDescription());
+        if (missingItem.getCategory() != null)
+            spinner.setSelection(getPreselectPosition(missingItem.getCategory()));
+    }
+
+    private int getPreselectPosition(String categoryName) {
+        List<CategoryItem> categoryItemList = RealmDatabase.getAllCategories(this);
+        int position = 0;
+        for (int i = 0; i < categoryItemList.size(); i++) {
+            if (categoryName.equals(categoryItemList.get(i).getCategoryName())) {
+                position = i;
+                break;
+            }
+        }
+        return position;
+    }
+
+    private boolean validate(String name, String description){
+        if (name.isEmpty()){
+            tilName.setError(getString(R.string.empty_entry));
+            tilName.setErrorEnabled(true);
+        } else {
+            tilName.setError(null);
+            tilName.setErrorEnabled(false);
+        }
+        if (description.isEmpty()){
+            tilDescription.setError(getString(R.string.empty_entry));
+            tilDescription.setErrorEnabled(true);
+        } else {
+            tilDescription.setError(null);
+            tilDescription.setErrorEnabled(false);
+        }
+        return !name.isEmpty() && !description.isEmpty();
+    }
+
+
 }
