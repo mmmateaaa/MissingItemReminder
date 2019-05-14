63d3ff902695cfc96780c18708ed02afe994b4b9
Matea <sim.mat06@gmail.com>
2019-05-14 09:26:04 +0200

    Describe Missing Item Fragment fix

--- app/src/main/java/mat06/sim/missingitemreminder/AddItemActivity.java
+++ app/src/main/java/mat06/sim/missingitemreminder/AddItemActivity.java
@@ -2,17 +2,25 @@ package mat06.sim.missingitemreminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import mat06.sim.missingitemreminder.database.RealmDatabase;
 import mat06.sim.missingitemreminder.fragments.DescribeMissingItemFragment;
 import mat06.sim.missingitemreminder.models.MissingItem;
 
 public class AddItemActivity extends AppCompatActivity {
-
+    public static final String EXTRA_ID = "EXTRA_ID";
     private MissingItem item;
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        long id = getIntent().getLongExtra(EXTRA_ID, -1L);
+        if (id != -1) {
+            item = RealmDatabase.queryMissingItemById(id);
+        } else
+            item = new MissingItem(System.currentTimeMillis());
 
         loadDescribeMissingItemFragment();
     }
